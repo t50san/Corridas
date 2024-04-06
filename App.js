9e9048f3942ced8e6fa76f24c2a53d1b936d592e
t50san <tamires.engp@gmail.com>
2024-04-06 18:03:24 -0300

    copia do codigo completo app.js e custom.js

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { ScrollView, View } from 'react-native';
-import { Container } from './app/src/styles/custom';
+import { ScrollView } from 'react-native';
+import { Container, Logo, ImageLogo, List, RowData, Bmi, Classification } from './app/src/styles/custom';
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1}}>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <Container>
+
+        { /* Carregar a logo */}
+        <Logo>
+          <ImageLogo source={require('./app/assets/logo.png')} />
+        </Logo>
+
+        {/* Criar a lista de classificação do IMC */}
+        <List>
+          <RowData>
+            <Bmi>
+              Menor que 18,5
+            </Bmi>
+            <Classification>
+              Magreza
+            </Classification>
+          </RowData>
+
+          <RowData>
+            <Bmi>
+              Entre 18,5 e 24,9
+            </Bmi>
+            <Classification>
+              Normal
+            </Classification>
+          </RowData>
+
+          <RowData>
+            <Bmi>
+              Entre 25,0 e 29,9
+            </Bmi>
+            <Classification>
+              Sobrepeso
+            </Classification>
+          </RowData>
+
+          <RowData>
+            <Bmi>
+              Entre 30,0 e 39,9
+            </Bmi>
+            <Classification>
+              Obesidade
+            </Classification>
+          </RowData>
+
+          <RowData>
+            <Bmi>
+              Maior que 40,0
+            </Bmi>
+            <Classification>
+              Obesidade Grave
+            </Classification>
+          </RowData>
+        </List>
+
       </Container>
     </ScrollView>
-    
   );
 }
--- app/src/styles/custom.js
+++ app/src/styles/custom.js
@@ -1,22 +1,37 @@
-import styled from "styled-components"  ;
+import styled from "styled-components";
 
-//export para usar no outro arquivo, o app.js
 export const Container = styled.SafeAreaView`
-    //tornar um item flexível dentro do container, valor 1 para ocupar todo o espaço da tela
-    flex: 1; 
+    flex: 1;
     align-items: center;
     justify-content: center;
+    background-color: #0b0e20;
 `;
 
 export const Logo = styled.View`
     padding: 30px 0;
 `;
 
-export const ImageLogo = styled.View`
-    padding: 30px 0;
+export const ImageLogo = styled.Image`
+    width: 100px;
+    height: 100px;
 `;
 
-export const TextExe = styled.Text`
-    padding: 20px;
-    font-size: 26px;
+export const List = styled.View`
+    width: 100%;
+`;
+
+export const RowData = styled.View`
+    background-color: #1d1e31;
+    padding: 10px;
+    margin: 5px 20px;
+    border-radius: 6px;
+    flex-direction: row;
+    justify-content: flex-start;
+`;
+
+export const Bmi = styled.Text`
+    flex: 1;
+    flex-direction: column;
+    font-size: 16px;
+    color: #8086c5;    
 `;
