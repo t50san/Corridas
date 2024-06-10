9b88936e1ed250b36f17b1ee6690dc1137187419
t50san <tamires.engp@gmail.com>
2024-06-09 21:39:09 -0300

    trying to add the repository again

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   const handleCadastro = () => {
